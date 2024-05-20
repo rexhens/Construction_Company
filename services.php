6b5ef896ea499f22415b7857755a6931941c21c2
maidadaulle <mdaulle22@epoka.edu.al>
2024-05-20 23:57:40 +0200

    services

---
+++ services.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <?php
+      include_once("head.html");
+      ?>
+   </head>
+   <body>
+      <div class="services_section layout_padding">
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-sm-12">
+                  <h1 class="services_taital">Our Services</h1>
+               </div>
+            </div>
+            <div class="services_section_2">
+               <div class="row">
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="box_main active">
+                        <div class="service_img"><img src="images/icon-1.png"></div>
+                        <h4 class="development_text">Construction Services</h4>
+                        <p class="services_text">With a focus on precision, innovation, and efficiency, we specialize in constructing residential, commercial, and industrial projects of all scales. From concept to completion, our team of experienced professionals ensures that every detail is meticulously executed, delivering superior quality and exceeding expectations. </p>
+                        <div class="readmore_bt"><a href="#">Read More</a></div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="box_main">
+                        <div class="service_img"><img src="images/icon-2.png"></div>
+                        <h4 class="development_text">Building Modeling</h4>
+                        <p class="services_text">Building modeling is the transformative process of translating architectural designs into digital representations, offering a realistic preview of structures before they are constructed. Utilizing advanced software and technologies, construction companies create detailed 3D models that simulate the layout, design, and functionality of buildings.</p>
+                        <div class="readmore_bt"><a href="#">Read More</a></div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="box_main">
+                        <div class="service_img"><img src="images/icon-3.png"></div>
+                        <h4 class="development_text">Pre construction</h4>
+                        <p class="services_text">It involves a comprehensive set of activities and analyses to lay the groundwork for a successful construction project. During this phase, construction companies conduct feasibility studies, site evaluations, budget estimations, and regulatory assessments to ensure that all aspects of the project are thoroughly considered and coordinated. Pre-construction also involves establishing timelines, procurement strategies, and risk management plans to minimize potential delays and disruptions.</p>
+                        <div class="readmore_bt"><a href="#">Read More</a></div>
+                     </div>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                     <div class="box_main">
+                        <div class="service_img"><img src="images/icon-4.png"></div>
+                        <h4 class="development_text">Management</h4>
+                        <p class="services_text">Construction project management involves overseeing every aspect of a building project from inception to completion. It's about orchestrating a symphony of tasks, teams, and resources to ensure that projects are delivered on time, within budget, and to the highest quality standards. </p>
+                        <div class="readmore_bt"><a href="#">Read More</a></div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- services section end -->
+      <!-- footer section start -->
+   </body>
+   <?php
+   include_once("footer.html");
+   ?>
+</html>
