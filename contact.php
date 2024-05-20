b15454a0e9f6f4cf0269262b6f7e46e40c7d5d7b
maidadaulle <mdaulle22@epoka.edu.al>
2024-05-21 00:14:08 +0200

    contact

---
+++ contact.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <?php
+      include_once("head.html")
+      ?>
+</head>
+<body>
+      <div class="contact_section layout_padding">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <h1 class="contact_taital">Contact Us</h1>
+               </div>
+            </div>
+         </div>
+         <div class="container-fluid">
+            <div class="contact_section_2">
+               <div class="row">
+                  <div class="col-md-6">
+                     <form action="">
+                        <div class="mail_section_1">
+                           <input type="text" class="mail_text" placeholder="Name" name="Name">
+                           <input type="text" class="mail_text" placeholder="Phone Number" name="Phone Number"> 
+                           <input type="text" class="mail_text" placeholder="Email" name="Email">
+                           <textarea class="massage-bt" placeholder="Massage" rows="5" id="comment" name="Massage"></textarea>
+                           <div class="send_bt"><a href="#">SEND</a></div>
+                        </div>
+                     </form>
+                  </div>
+                  <div class="col-md-6 padding_left_15">
+                     <div class="contact_img"><img src="images/contact-img.png"></div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="map_main">
+            <div class="map-responsive">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d47934.06594205342!2d19.776623505268535!3d41.33324180436733!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1350310470fac5db%3A0x40092af10653720!2sTirana%2C%20Albania!5e0!3m2!1sen!2s!4v1716242849116!5m2!1sen!2s" width="600" height="600" frameborder="0" style="border:0; width: 100%;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+            </div>
+         </div>
+      </div>
+   </body>
+   <?php
+   include_once("footer.html");
+   ?>
+</html>
